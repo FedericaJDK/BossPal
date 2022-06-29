b02fb283ea354374224a43318f4320b069988481
Federica D'Alvano <federicajosefina@gmail.com>
2022-06-29 14:48:31 -0400

    added the View controllers for the Habit tracker and added their actions and outlets.

---
+++ BossPal/AddHabitViewController.swift
@@ -0,0 +1,37 @@
+//
+//  AddHabitViewController.swift
+//  BossPal
+//
+//  Created by Federica D’Alvano on 6/29/22.
+//
+
+import UIKit
+
+class AddHabitViewController: UIViewController {
+
+    @IBOutlet weak var priorityLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func habitText(_ sender: Any) {
+    }
+    @IBAction func switchPriority(_ sender: Any) {
+    }
+    @IBAction func addButton(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ BossPal/CompleteHabitViewController.swift
@@ -0,0 +1,32 @@
+//
+//  CompleteHabitViewController.swift
+//  BossPal
+//
+//  Created by Federica D’Alvano on 6/29/22.
+//
+
+import UIKit
+
+class CompleteHabitViewController: UIViewController {
+
+    @IBOutlet weak var goalNameLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func completeButton(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
